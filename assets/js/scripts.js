ecda119fd47c2fd647d8ed4ace9091a85f364b20
Dresdain <dresdain@live.com>
2018-05-23 02:35:16 +0800

    Mobile nav updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,3 +13,47 @@ $(document).mouseup(function(e) {
     	$('.has-sub').removeClass('opened');   
     }
 });
+
+
+$('.mobile-nav-trigger').on('click', function(){
+    $('.mobile-nav').show();
+    // $('body').css({
+    //     'position': 'fixed',
+    //     'overflow': 'hidden'
+    // });
+	$( '.mobile-nav' ).animate({
+        'right':'0px'
+    }, {
+        duration: 400,
+        easing: 'easeOutQuart'
+    });  
+});
+
+function hideMobileNav(){
+    // $('body').css({
+    //     'position': 'relative',
+    //     'overflow': 'auto'
+    // });
+    $( '.mobile-nav' ).animate({
+        'right':'-300px'
+    }, {
+        duration: 400,
+        easing: 'easeOutQuart',
+        complete: function() {
+            $('.mobile-nav').hide();
+        }
+    }); 
+}
+
+$(document).mouseup(function(e) {
+    var container = $(".mobile-nav, .mobile-nav-trigger");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) {   
+        hideMobileNav();
+    }
+});
+
+$('.mobile-nav-close-trigger').on('click', function(){
+    hideMobileNav();
+});
