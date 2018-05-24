11e99ce8a7a21769561d4c6fd4c14c358079e93a
xydfayt <xydfayt@yahoo.com>
2018-05-25 04:35:02 +0800

    Videos Page

--- page-brochures.php
+++ page-brochures.php
@@ -3,52 +3,47 @@
 ?>
 <section class="white-block container brochures-section">
           <div class="row">
+            <?php  
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+            $args = array(
+                'post_type' => 'brochures',
+                'paged' => $paged,
+                'posts_per_page' => 9,
+                'order' => 'DESC',
+                'orderby' => 'post_date'
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts()){
+              while($query->have_posts()){
+                $query->the_post();
+                $image = get_field('image');
+                $file = get_field('file');
+            ?>
             <div class="col-md-4">
               <div class="panel panel-default panel-brochures">
                   <div class="panel-body">
                     <p class="brochure-title">
-                      Title of the Brochure or Document
+                      <?php the_title(); ?>
                     </p>
                     <div class="brochure-image">
-                      <img src="./assets/img/brochure.jpg">
+                      <img src="<?php echo $image['url']; ?>">
                     </div>
                   </div>
+                  <a href="<?php echo $file['url']; ?>">
                   <div class="panel-footer">
                     Download Here
                   </div>
+                  </a>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="panel panel-default panel-brochures">
-                  <div class="panel-body">
-                    <p class="brochure-title">
-                      Title of the Brochure or Document
-                    </p>
-                    <div class="brochure-image">
-                      <img src="./assets/img/brochure.jpg">
-                    </div>
-                  </div>
-                  <div class="panel-footer">
-                    Download Here
-                  </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="panel panel-default panel-brochures">
-                  <div class="panel-body">
-                    <p class="brochure-title">
-                      Title of the Brochure or Document
-                    </p>
-                    <div class="brochure-image">
-                      <img src="./assets/img/brochure.jpg">
-                    </div>
-                  </div>
-                  <div class="panel-footer">
-                    Download Here
-                  </div>
-              </div>
-            </div>
+            <?php
+              }
+              }
+              wp_reset_query();
+            ?>
           </div>
+          <?php get_template_part('pagination'); ?> 
 </section>
 <?php
 	get_footer();
--- page-videos.php
+++ page-videos.php
@@ -1,51 +1,45 @@
 <?php  
 	get_header();
 ?>
-
-
-    <section class="white-block container videos-section">
+<section class="white-block container videos-section">
           <div class="row">
+            <?php  
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+            $args = array(
+                'post_type' => 'videos',
+                'paged' => $paged,
+                'posts_per_page' => 9,
+                'order' => 'DESC',
+                'orderby' => 'post_date'
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts()){
+              while($query->have_posts()){
+                $query->the_post();
+                $image = get_field('thumbnail');
+            ?>
             <div class ="col-md-4">
               <div class="video-box">
                 <div class="video-thumbnail">
-                  <a href="#" class ="video-link">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/galleries1.jpg" class ="video">
+                  <a href="<?php the_permalink(); ?>" class ="video-link">
+                  <img src="<?php echo $image['url']; ?>" class ="video">
                   </a>
                   <div class="text-box">
-                    <p class ="video-title"><a href="">Title of the Video</a></p>
-                    <p class ="video-date">April 2018</p>
+                    <p class ="video-title"><a href=""><?php the_title(); ?></a></p>
+                    <p class ="video-date"><?php echo get_field('date'); ?></p>
                   </div>
                 </div>
               </div>
             </div>
-            <div class ="col-md-4">
-              <div class="video-box">
-                <div class="video-thumbnail">
-                  <a href="#" class ="video-link">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/galleries2.jpg" class ="video">
-                  </a>
-                  <div class="text-box">
-                    <p class ="video-title"><a href="">Title of the Video</a></p>
-                    <p class ="video-date">April 2018</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class ="col-md-4">
-              <div class="video-box">
-                <div class="video-thumbnail">
-                  <a href="#" class ="video-link">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/galleries3.jpg" class ="video">
-                  </a>
-                  <div class="text-box">
-                    <p class ="video-title"><a href="">Title of the Video</a></p>
-                    <p class ="video-date">April 2018</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <?php
+              }
+              }
+              wp_reset_query();
+            ?>
           </div>
-    </section>
+          <?php get_template_part('pagination'); ?> 
+</section>
 
 <?php
 	get_footer();
