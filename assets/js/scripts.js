cd05dc60071e8ac272557bc587510ad90651b3ba
xydfayt <xydfayt@yahoo.com>
2018-05-25 19:31:52 +0800

    Scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,113 +1,95 @@
- $('.has-sub').on('click', function(){
-    $('.submenu', this).css('display', 'block');  
-    $(this).addClass('opened');  
-});
- 
-$(document).on('mouseup', function(e) {
-    var container = $(".submenu");
+(function($) {
+    $('.has-sub').on('click', function(){
+    	$('.submenu', this).css('display', 'block');  
+    	$(this).addClass('opened');  
+    });
+     
+    $(document).mouseup(function(e) {
+    	var container = $(".submenu");
 
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) 
-    {
-        $('.submenu').hide();   
-        $('.has-sub').removeClass('opened');   
-    }
-});
-
-
-$('.mobile-nav-trigger').on('click', function(){
-    $('.mobile-nav').show();
-    // $('body').css({
-    //     'position': 'fixed',
-    //     'overflow': 'hidden'
-    // });
-    $( '.mobile-nav' ).animate({
-        'right':'0px'
-    }, {
-        duration: 400,
-        easing: 'easeOutQuart'
-    });  
-});
-
-function hideMobileNav(){
-    // $('body').css({
-    //     'position': 'relative',
-    //     'overflow': 'auto'
-    // });
-    $( '.mobile-nav' ).animate({
-        'right':'-500px'
-    }, {
-        duration: 500,
-        easing: 'easeOutQuart',
-        complete: function() {
-            $('.mobile-nav').hide();
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+        	$('.submenu').hide();   
+        	$('.has-sub').removeClass('opened');   
         }
-    }); 
-}
+    });
 
-$(document).mouseup(function(e) {
-    var container = $(".mobile-nav, .mobile-nav-trigger");
 
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) {   
-        hideMobileNav();
-    }
-});
+    $('.mobile-nav-trigger').on('click', function(){
+        $('.mobile-nav').show();
+        // $('body').css({
+        //     'position': 'fixed',
+        //     'overflow': 'hidden'
+        // });
+    	$( '.mobile-nav' ).animate({
+            'right':'0px'
+        }, {
+            duration: 400,
+            easing: 'easeOutQuart'
+        });  
+    });
 
-$('.mobile-nav-close-trigger').on('click', function(){
-    hideMobileNav();
-});
-
-var x, i, j, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  /*for each element, create a new DIV that will act as the selected item:*/
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 0; j < selElmnt.length; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /*when an item is clicked, update the original select box,
-        and the selected item:*/
-        var y, i, k, s, h;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
+    function hideMobileNav(){
+        // $('body').css({
+        //     'position': 'relative',
+        //     'overflow': 'auto'
+        // });
+        $( '.mobile-nav' ).animate({
+            'right':'-500px'
+        }, {
+            duration: 500,
+            easing: 'easeOutQuart',
+            complete: function() {
+                $('.mobile-nav').hide();
             }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
+        }); 
+    }
+
+    $(document).mouseup(function(e) {
+        var container = $(".mobile-nav, .mobile-nav-trigger");
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) {   
+            hideMobileNav();
         }
-        h.click();
     });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-      /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
+
+    $('.mobile-nav-close-trigger').on('click', function(){
+        hideMobileNav();
     });
-}
+
+    $("#contact-form").validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {required: true},
+        },
+        submitHandler: function(form) {
+            $('#contact-submit').text('Sending...').attr('disabled', true);
+            var data = $(form).serialize();
+
+            $.ajax({
+                type: "POST",
+                url: udlap_contact_form_vars.ajaxUrl,
+                data: $(form).serialize() + '&nonce=' + udlap_contact_form_vars.nonce + '&action=udlap_contact_form',
+                success: function () {
+                    $('#contact-success').show();
+                    $('#contact-submit').text('Send').attr('disabled', false);
+                }
+            });
+
+            return false;
+        }
+    });
+   
+})( jQuery );
+
 function closeAllSelect(elmnt) {
   /*a function that will close all select boxes in the document,
   except the current select box:*/
