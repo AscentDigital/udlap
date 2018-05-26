1cf991b8aee57add4178355d6b4cbdfaad9081d4
xydfayt <xydfayt@yahoo.com>
2018-05-27 00:19:18 +0800

    Footer Update

--- footer.php
+++ footer.php
@@ -6,12 +6,12 @@
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logotop.png" alt="">
   </div>
   <div class="f-nav container">
-    <a href="./index.html">Home</a>
+    <a href="<?php echo get_site_url(); ?>">Home</a>
     <a href="./about.html">About</a>
     <a href="./programs.html">Programs</a>
     <a href="./student-life.html">Student Life</a>
     <a href="./financial-aid.html">Financial Aid</a>
-    <a href="./updates.html">Updates</a>
+    <a href="<?php echo get_site_url(); ?>/updates.html">Updates</a>
     <a href="./contact.html">Contact</a>
   </div>  
   <div class="footer">
